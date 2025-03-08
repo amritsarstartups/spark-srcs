3e8ec0f4fc41aa8872a7dde8ef1017ac53f4c57a
Rishabh Mahajan <rishabh@statusbrew.com>
2025-03-08 18:24:23 +0530

    task(sfl): Bootstrapping next lib

---
+++ libs/sfl/ui/src/lib/ui.tsx
@@ -0,0 +1,11 @@
+import styles from './ui.module.css';
+
+export function SflUi() {
+  return (
+    <div className={styles['container']}>
+      <h1>Welcome to Ui!</h1>
+    </div>
+  );
+}
+
+export default SflUi;
